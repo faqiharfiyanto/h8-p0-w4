b0aa3ed1154c62008aa886b685ab6b577efcaa07
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-02-26 11:56:56 +0700

    modified exercise 14

--- p0-w4-ex14.js
+++ p0-w4-ex14.js
@@ -6,7 +6,13 @@ function naikAngkot(arrPenumpang) {
         angkotObj.penumpang = arrPenumpang[i][0]
         angkotObj.naikDari = arrPenumpang[i][1]
         angkotObj.tujuan = arrPenumpang[i][2]
-        angkotObj.harga = (rute.indexOf(angkotObj.tujuan) - rute.indexOf(angkotObj.naikDari)) * 2000
+        var harga = (rute.indexOf(angkotObj.tujuan) - rute.indexOf(angkotObj.naikDari)) * 2000
+        if(harga < 0) {
+            angkotObj.harga = harga * -1
+        }
+        else {
+            angkotObj.harga = harga
+        }
         temp.push(angkotObj)
         // console.log(temp)
     }
@@ -19,3 +25,4 @@ function naikAngkot(arrPenumpang) {
   //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
   
   console.log(naikAngkot([])); //[]
+  console.log(naikAngkot([['Dimitri', 'F', 'B'], ['Icha', 'B', 'A']]));
