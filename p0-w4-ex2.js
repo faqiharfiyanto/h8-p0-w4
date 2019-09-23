b1112b2ca841acbcc94135c7529b3aba66ad80da
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-09-23 11:07:14 +0700

    update exercise 2

--- p0-w4-ex2.js
+++ p0-w4-ex2.js
@@ -1,16 +1,14 @@
 function fpb(angka1, angka2) {
-    var faktorBersama = []
-    for (var i=1; i<=angka1 || i<= angka2; i++) {
-      if (angka1%i==0 && angka2%i==0) {
-        faktorBersama.push(i)
-      }
+  for (var i=angka1; i>0; i--) {
+    if (angka1%i==0 && angka2%i==0) {
+      return i
     }
-    return faktorBersama[faktorBersama.length-1]
   }
-  
-  // TEST CASES
-  console.log(fpb(12, 16)); // 4
-  console.log(fpb(50, 40)); // 10
-  console.log(fpb(22, 99)); // 11
-  console.log(fpb(24, 36)); // 12
-  console.log(fpb(17, 23)); // 1
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
